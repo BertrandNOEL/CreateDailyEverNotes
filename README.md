759a781edfd048c325f0e6b136f426c9ec7c75ca
Bertrand NOEL <bertrand.noel.88@gmail.com>
2016-05-27 08:15:47 +0200

    - Adds templates - Adds reminders - Notebook name as argument

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # CreateDailyEverNotes
-Create an empty note in the specified Evernote notebook.
+
+Script to create an Evernote note based on a template. It can be used to ease keeping a daily worklog, or to create daily todo list. A reminder can be defined.
 
 
-With the following cron job, it can be used to ease keeping a daily journal or worklog.
+For example, use the following cron job to create an empty note on every week days, and a reminder to fill it at 09:00.
+
 ```bash
 # At 05:00 every workday
-0 5 * * Mon-Fri python /home/user/CreateDailyEverNotes/create_daily_evernote.py
+0 5 * * Mon-Fri python /home/user/CreateDailyEverNotes/create_daily_evernote.py --notebook "Worklog" --template work.html --reminder-time 09:00
 ```
 
 # TODO
-- export template of the note to an external file
+* [x] export template of the note to an external file
+* [x] ability to set reminders to notes
+* [ ] ability to create weekly and monthly note
+* [ ] clean previous reminders
+* [ ] remove empty notes
+* [ ] put code on Google App Engine or Google Apps script?
--- create_daily_evernote.py
+++ create_daily_evernote.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python
+
+import argparse
 import sys
+import os
+import time
 from datetime import datetime
 # To get here:
 # https://github.com/evernote/evernote-sdk-python
@@ -9,47 +14,58 @@ from evernote.api.client import EvernoteClient
 
 # To get here:
 # https://dev.evernote.com/doc/articles/dev_tokens.php
-auth_token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
-# Name of the notebook where notes will be created
-notebook_name = "Journal de Rorschach"
 # https://docs.python.org/3.5/library/datetime.html#strftime-strptime-behavior
 title_format_date = "%Y/%m/%d %A"
 
 
-client = EvernoteClient(token=auth_token, sandbox=False)
-note_store = client.get_note_store()
-notebooks = note_store.listNotebooks()
-def findNotebook(notebooks):
-    for notebook in notebooks:
-        if notebook.name == notebook_name:
-            return notebook
-    return None
-notebook_to_use = findNotebook(notebooks)
-if not notebook_to_use:
-    print("Error. The notebook '%s' does not exists" % notebook_name)
-    sys.exit(-1)
+def get_args():
+	parser = argparse.ArgumentParser(description='Create an Evernote note using template')
+	parser.add_argument('--notebook', required=True, help='Name of the notebook where notes will be created')
+	parser.add_argument('--template', required=True, type=argparse.FileType('r'), help='Template for the message to send')
+	parser.add_argument('--reminder-time', required=False, help='Time to set the reminder to')
+	return parser.parse_args()
 
-print("Going to create the note in notebook '%s'" % notebook_name)
-note = Types.Note()
-# Link note to notebook
-note.notebookGuid = notebook_to_use.guid
-note.title = datetime.now().strftime(title_format_date)
-# Empty message
-note.content = '<?xml version="1.0" encoding="UTF-8"?>'
-note.content += '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'
-note.content += '<en-note>'
-note.content += '<u>TODO</u><br/>'
-note.content += '<br/>'
-note.content += '- <br/>'
-note.content += '- <br/>'
-note.content += '- <br/>'
-note.content += '- <br/>'
-note.content += '<br/><br/>'
-note.content += '<hr></hr><br/>'
-note.content += '<u>Notes</u><br/>'
-note.content += '<br/><br/><br/><br/><br/><br/><br/>'
-note.content += '</en-note>'
+def main():
+	args = get_args()
+	
+	client = EvernoteClient(token=auth_token, sandbox=False)
+	note_store = client.get_note_store()
+	notebooks = note_store.listNotebooks()
+	def findNotebook(notebooks, name):
+		for notebook in notebooks:
+			if notebook.name == name:
+				return notebook
+		return
+	notebook_to_use = findNotebook(notebooks, args.notebook)
+	if not notebook_to_use:
+		print("Error. The notebook '%s' does not exists" % args.notebook)
+		sys.exit(-1)
+	
+	print("Going to create the note in notebook '%s'" % notebook_to_use.name)
+	note = Types.Note()
+	# Link note to notebook
+	note.notebookGuid = notebook_to_use.guid
+	note.title = datetime.now().strftime(title_format_date)
+	
+	# Message
+	print("Using %s as a template for the note" % args.template.name)
+	lines = args.template.readlines()
+	note.content = ''.join(lines)
+	
+	if(args.reminder_time):
+		hour, minute = args.reminder_time.split(":")
+		when = datetime.now().replace(hour=int(hour), minute=int(minute), second=00)
+		note.attributes = Types.NoteAttributes()
+		# To define the relative order in the reminders list
+		#note.attributes.reminderOrder = 1
+		# time.mktime -> gives unix timestamp in seconds (considering local tz)
+		# reminderTime -> is expecting milliseconds
+		note.attributes.reminderTime = time.mktime(when.timetuple()) * 1000
+	
+	createdNote = note_store.createNote(note)
+	print("Successfully created note '%s'" % note.title)
 
-created_note = note_store.createNote(note)
-print("Successfully created note '%s'" % note.title)
+if __name__ == "__main__":
+	main()
