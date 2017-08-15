c404813b0924db8562ace9a5e262d6826e925ebe
Bertrand NOEL <bertrand.noel.88@gmail.com>
2017-08-15 16:05:15 +0200

    Adds function to clear reminders

--- README.md
+++ README.md
@@ -7,13 +7,20 @@ For example, use the following cron job to create an empty note on every week da
 
 ```bash
 # At 05:00 every workday
-0 5 * * Mon-Fri python /home/user/CreateDailyEverNotes/create_daily_evernote.py --notebook "Worklog" --template work.html --reminder-time 09:00
+0 5 * * Mon-Fri python /home/user/CreateDailyEverNotes/create_daily_evernote.py create-note --notebook "Worklog" --template work.html --reminder-time 09:00
+```
+
+Use this cron to clear reminders from Notes
+
+```bash
+# At 04:00 on Sunday
+0 4 * * Sun python /home/user/CreateDailyEverNotes/create_daily_evernote.py clear-reminders --notebook "Worklog"
 ```
 
 # TODO
 * [x] export template of the note to an external file
 * [x] ability to set reminders to notes
+* [x] clean previous reminders
 * [ ] ability to create weekly and monthly note
-* [ ] clean previous reminders
 * [ ] remove empty notes
 * [ ] put code on Google App Engine or Google Apps script?
--- create_daily_evernote.py
+++ create_daily_evernote.py
@@ -11,6 +11,7 @@ from datetime import datetime
 # > sudo python setup.py install
 import evernote.edam.type.ttypes as Types
 from evernote.api.client import EvernoteClient
+import evernote.edam.notestore.NoteStore as NoteStore
 
 # To get here:
 # https://dev.evernote.com/doc/articles/dev_tokens.php
@@ -21,15 +22,27 @@ title_format_date = "%Y/%m/%d %A"
 
 
 def get_args():
+	if len(sys.argv) < 2:
+		# argparse does not handle when no subcommand is passed
+		print("error, invalid choice. Choose from 'create-note', 'clear-reminders'")
+		sys.exit(-1)
+	
 	parser = argparse.ArgumentParser(description='Create an Evernote note using template')
-	parser.add_argument('--notebook', required=True, help='Name of the notebook where notes will be created')
-	parser.add_argument('--template', required=True, type=argparse.FileType('r'), help='Template for the message to send')
-	parser.add_argument('--reminder-time', required=False, help='Time to set the reminder to')
+	subparsers = parser.add_subparsers()
+	
+	parser_a = subparsers.add_parser('create-note')
+	parser_a.add_argument('--notebook', required=True, help='Name of the notebook where notes are stored')
+	parser_a.add_argument('--template', required=True, type=argparse.FileType('r'), help='Template for the message to send')
+	parser_a.add_argument('--reminder-time', required=False, help='Time to set the reminder to')
+	parser_a.set_defaults(func=create_note)
+	
+	parser_b = subparsers.add_parser('clear-reminders')
+	parser_b.add_argument('--notebook', required=True, help='Name of the notebook where notes are stored')
+	parser_b.set_defaults(func=clear_reminders)
+	
 	return parser.parse_args()
 
-def main():
-	args = get_args()
-	
+def init(notebook_name):
 	client = EvernoteClient(token=auth_token, sandbox=False)
 	note_store = client.get_note_store()
 	notebooks = note_store.listNotebooks()
@@ -38,10 +51,17 @@ def main():
 			if notebook.name == name:
 				return notebook
 		return
-	notebook_to_use = findNotebook(notebooks, args.notebook)
+	notebook_to_use = findNotebook(notebooks, notebook_name)
 	if not notebook_to_use:
-		print("Error. The notebook '%s' does not exists" % args.notebook)
+		print("Error. The notebook '%s' does not exists" % notebook_name)
 		sys.exit(-1)
+	
+	return note_store, notebook_to_use
+
+def create_note(args):
+	note_store, notebook_to_use = init(args.notebook)
+	template = args.template
+	reminder_time = args.reminder_time
 	
 	print("Going to create the note in notebook '%s'" % notebook_to_use.name)
 	note = Types.Note()
@@ -50,22 +70,58 @@ def main():
 	note.title = datetime.now().strftime(title_format_date)
 	
 	# Message
-	print("Using %s as a template for the note" % args.template.name)
-	lines = args.template.readlines()
+	print("Using %s as a template for the note" % template.name)
+	lines = template.readlines()
 	note.content = ''.join(lines)
 	
-	if(args.reminder_time):
-		hour, minute = args.reminder_time.split(":")
-		when = datetime.now().replace(hour=int(hour), minute=int(minute), second=00)
+	if reminder_time:
+		# TODO this way of handling an hour is very bad. 99:99 or aa:bb would work
+		hour, minute = reminder_time.split(":")
+		if not hour or not minute:
+			print("Error, could not parse reminder_time %s. Must be of form hh:mm" % reminder_time)
+			sys.exit(-1)
+		when = datetime.now().replace(hour=int(hour), minute=int(minute), second=00, microsecond=000)
 		note.attributes = Types.NoteAttributes()
 		# To define the relative order in the reminders list
 		#note.attributes.reminderOrder = 1
 		# time.mktime -> gives unix timestamp in seconds (considering local tz)
 		# reminderTime -> is expecting milliseconds
-		note.attributes.reminderTime = time.mktime(when.timetuple()) * 1000
+		note.attributes.reminderTime = int(time.mktime(when.timetuple()) * 1000)
 	
 	createdNote = note_store.createNote(note)
 	print("Successfully created note '%s'" % note.title)
+
+def clear_reminders(args):
+	note_store, notebook_to_use = init(args.notebook)
+	print("Clear reminders")
+	
+	# We first get Note metadata of Notes from the Notebook
+	# through a Filter
+	filter = NoteStore.NoteFilter()
+	filter.notebookGuid = notebook_to_use.guid
+	
+	spec = NoteStore.NotesMetadataResultSpec()
+	# Get the 20 most recent
+	# TODO get only the ones with reminder
+	result = note_store.findNotesMetadata(filter, 0, 20, spec)
+	print("Going to remove reminder for %s notes" % len(result.notes))
+	for noteMetadata in result.notes:
+		# Now we get the Note object, without retrieving any content along...
+		wholeNote = note_store.getNote(noteMetadata.guid, False, False, False, False)
+		# ... and reset the reminder attributes if they were set
+		if wholeNote.attributes.reminderTime or \
+				wholeNote.attributes.reminderOrder or \
+				wholeNote.attributes.reminderDoneTime:
+			# Updating
+			wholeNote.attributes.reminderTime = None
+			wholeNote.attributes.reminderOrder = None
+			wholeNote.attributes.reminderDoneTime = None
+			note_store.updateNote(wholeNote)
+	print("Done")
+
+def main():
+	args = get_args()
+	args.func(args)
 
 if __name__ == "__main__":
 	main()
